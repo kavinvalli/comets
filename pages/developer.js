96e74ab7dee615350e1bcf239d37e38c86eae6e9
Kavin Valli <mail@kavin.me>
2022-07-31 23:25:09 +0530

    fix: prisma not defined

--- pages/developer.js
+++ pages/developer.js
@@ -1,4 +1,4 @@
-// import prisma from "../lib/prisma.ts";
+import prisma from "../lib/prisma.ts";
 import { useSession, getSession } from "next-auth/react";
 import { useState } from "react";
 import Link from "next/link";
